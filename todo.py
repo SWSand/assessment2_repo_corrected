ef3c21b2db659ec29305f5715b997c8c0f154b8f
Jose Sandoval <Software.Sandoval@gmail.com>
2024-07-13 11:49:55 -0500

    todo.py update mysql import

--- todo.py
+++ todo.py
@@ -1,63 +1,41 @@
 import logging
 import os
 from flask import Flask, jsonify, abort, request, make_response
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 from flask_cors import CORS
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Set up logging
-# logging.basicConfig(filename='application.log', level=logging.INFO)
-
-# Create a formatter that includes a timestamp
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-# Create a logger and configure it with the formatter
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-# Specify the directory path and create it if it doesn't exist
-log_directory = 'logs'  # Replace 'logs' with the desired directory name
+log_directory = 'logs'
 os.makedirs(log_directory, exist_ok=True)
-
-# Specify the log file path
 log_file_path = os.path.join(log_directory, 'application.log')
-
-# Create a file handler and add it to the logger
 file_handler = logging.FileHandler(log_file_path, mode='a')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-
-# Log a sample message
 logger.info('This is a sample log message to test.')
 
 app = Flask(__name__)
 CORS(app)
 
 # Configuring MySQL database
-app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
-app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE')
-app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('MYSQL_DATABASE_PORT'))
-mysql = MySQL()
-mysql.init_app(app)
-connection = mysql.connect()
-connection.autocommit(True)
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_DATABASE_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DATABASE')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_DATABASE_PORT'))
+mysql = MySQL(app)
+
+connection = mysql.connection
 cursor = connection.cursor()
 
-# Function to initialize to-do database
 def init_todo_db():
-    """Function to initialize the to-do list database by creating and populating the table."""
-    # Get the database name from the environment variable
     db_name = os.getenv('MYSQL_DATABASE')
-    
-    # Drop table if it exists
     drop_table = f'DROP TABLE IF EXISTS {db_name}.todos;'
-
-    # Create new table
     todos_table = f"""
     CREATE TABLE {db_name}.todos(
     task_id INT NOT NULL AUTO_INCREMENT,
@@ -67,8 +45,6 @@ def init_todo_db():
     PRIMARY KEY (task_id)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
     """
-    
-    # Insert data into table
     data = f"""
     INSERT INTO {db_name}.todos (title, description, is_done)
     VALUES
@@ -76,11 +52,10 @@ def init_todo_db():
         ("Infra and automation", "Just forgot. Need to revise again.", 0),
         ("CICD", "Learn more", 0);
     """
-    
     cursor.execute(drop_table)
     cursor.execute(todos_table)
     cursor.execute(data)
-# Logging helper functions
+
 def log_request_start():
     logging.info('Request started: %s %s', request.method, request.url)
 
@@ -93,7 +68,6 @@ def log_task_operation(operation):
 def log_request_error(error_code):
     logging.error('Request error: %s %s', error_code, request.url)
 
-# Flask request hooks
 @app.before_request
 def before_request():
     log_request_start()
@@ -103,46 +77,39 @@ def after_request(response):
     log_request_end()
     return response
 
-
-
 def get_all_tasks():
-    """Function to retrieve all tasks from the database."""
     query = "SELECT * FROM todos;"
     cursor.execute(query)
     result = cursor.fetchall()
-    tasks =[{'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])} for row in result]
+    tasks = [{'task_id': row[0], 'title': row[1], 'description': row[2], 'is_done': bool(row[3])} for row in result]
     return tasks
 
 def find_task(id):
-    """Function to find a task by its ID in the database."""
     query = f"SELECT * FROM todos WHERE task_id={id};"
     cursor.execute(query)
     row = cursor.fetchone()
     task = None
     if row is not None:
-        task = {'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])}
+        task = {'task_id': row[0], 'title': row[1], 'description': row[2], 'is_done': bool(row[3])}
     return task
 
 def insert_task(title, description):
-    """Function to insert a new task into the database."""
     insert = f"INSERT INTO todos (title, description) VALUES ('{title}', '{description}');"
     cursor.execute(insert)
     query = f"SELECT * FROM todos WHERE task_id={cursor.lastrowid};"
     cursor.execute(query)
     row = cursor.fetchone()
-    return {'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])}
+    return {'task_id': row[0], 'title': row[1], 'description': row[2], 'is_done': bool(row[3])}
 
 def change_task(task):
-    """Function to change the details of an existing task in the database."""
     update = f"UPDATE todos SET title='{task['title']}', description = '{task['description']}', is_done = {task['is_done']} WHERE task_id= {task['task_id']};"
     cursor.execute(update)
     query = f"SELECT * FROM todos WHERE task_id={task['task_id']};"
     cursor.execute(query)
     row = cursor.fetchone()
-    return {'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])}
+    return {'task_id': row[0], 'title': row[1], 'description': row[2], 'is_done': bool(row[3])}
 
 def remove_task(task):
-    """Function to remove a task from the database."""
     delete = f"DELETE FROM todos WHERE task_id= {task['task_id']};"
     cursor.execute(delete)
     query = f"SELECT * FROM todos WHERE task_id={task['task_id']};"
@@ -150,16 +117,12 @@ def remove_task(task):
     row = cursor.fetchone()
     return True if row is None else False
 
-
-# Set up Flask routes for API
 @app.route('/')
 def home():
-    """Home route that returns a welcome message."""
     return "Welcome to the to-do API Service"
 
 @app.route('/todos', methods=['GET'])
 def get_tasks():
-    """API route to retrieve all tasks."""
     try:
         tasks = get_all_tasks()
         return jsonify({'tasks': tasks})
@@ -169,7 +132,6 @@ def get_tasks():
 
 @app.route('/todos/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    """API route to retrieve a specific task by ID."""
     try:
         task = find_task(task_id)
         if task is None:
@@ -181,7 +143,6 @@ def get_task(task_id):
 
 @app.route('/todos', methods=['POST'])
 def add_task():
-    """API route to add a new task."""
     try:
         if not request.json or not 'title' in request.json:
             abort(400)
@@ -194,7 +155,6 @@ def add_task():
 
 @app.route('/todos/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
-    """API route to update an existing task."""
     try:
         task = find_task(task_id)
         if task is None:
@@ -213,7 +173,6 @@ def update_task(task_id):
 
 @app.route('/todos/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    """API route to delete a task."""
     try:
         task = find_task(task_id)
         if task is None:
@@ -230,13 +189,11 @@ def delete_task(task_id):
 
 @app.errorhandler(404)
 def not_found(error):
-    """Error handler for 404 errors."""
     log_request_error(404)
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.errorhandler(400)
 def bad_request(error):
-    """Error handler for 400 errors."""
     log_request_error(400)
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
